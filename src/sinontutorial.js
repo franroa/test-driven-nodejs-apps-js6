66606809ba437c7fefd5f6287595ddb5a64091ad
franroa <franciscoroaprieto@gmail.com>
2017-08-11 21:51:56 +0200

    added all test of sinon tutorial

--- src/sinontutorial.js
+++ src/sinontutorial.js
@@ -1,6 +1,8 @@
+import jQuery from 'jquery';
+
 let SinonTutorial = function () {
 
-    this.once = function(fn) {
+     this.once = function(fn) {
         var returnValue, called = false;
         return function () {
             if (!called) {
@@ -9,7 +11,29 @@ let SinonTutorial = function () {
             }
             return returnValue;
         };
+    };
+
+    this.getTodos = function (listId, callback) {
+        new jQuery().ajax(
+            '/todo/' + listId + '/items',
+            {
+                success: function (data) {
+                    callback(null, data);
+                }
+            }
+        );
+    };
+
+    this.throttle = function (callback) {
+        var timer;
+        return function () {
+            clearTimeout(timer);
+            var args = [].slice.call(arguments);
+            timer = setTimeout(function () {
+                callback.apply(this, args);
+            }, 100);
+        };
     }
-}
+};
 
 module.exports = SinonTutorial;
--- test/sinontutorial-test.js
+++ test/sinontutorial-test.js
@@ -10,20 +10,37 @@ chai.use(sinonChaiInOrder);
 
 describe('sinon tutorial', function () {
 
-    let sandbox;
-    let sinonTutorial;
+    let sandbox, sinonTutorial, xhr, requests, server, clock;
 
     beforeEach(function () {
         sinonTutorial = new SinonTutorial();
         sandbox = new sinon.sandbox.create();
+        xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
+        xhr.onCreate = function (req) { requests.push(req); };
     });
 
     afterEach(function () {
         sandbox.restore();
+        xhr.restore();
+    });
+
+    before(function () {
+        server = sinon.fakeServer.create();
+        clock = sinon.useFakeTimers();
+    });
+
+    after(function () {
+        clock.restore();
+        server.restore();
     });
 
     it('should pass the canary test', function () {
         expect(true).to.be.true
+    });
+
+    it('should pad a string', () => {
+        assert.equal('0foo', '0foo');
     });
 
     it('calls the original function', function () {
@@ -58,5 +75,49 @@ describe('sinon tutorial', function () {
         expect(spy).inOrder.to.have.been.calledWith(1)
             .subsequently.calledWith(2)
             .subsequently.calledWith(3);
+    });
+
+    it("returns the return value from the original function", function () {
+        var callback = sinon.stub().returns(42);
+        var proxy = sinonTutorial.once(callback);
+
+        assert.equal(proxy(), 42);
+    });
+
+    // it("makes a GET request for todo items", function () {
+    //     sinonTutorial.getTodos(42, sinon.spy());
+    //
+    //     assert.equal(requests.length, 1);
+    //     assert.match(requests[0].url, "/todo/42/items");
+    // });
+
+    // it("calls callback with deserialized data", function () {
+    //     var callback = sinon.spy();
+    //     sinonTutorial.getTodos(42, callback);
+    //
+    //     // This is part of the FakeXMLHttpRequest API
+    //     server.requests[0].respond(
+    //         200,
+    //         { "Content-Type": "application/json" },
+    //         JSON.stringify([{ id: 1, text: "Provide examples", done: true }])
+    //     );
+    //
+    //     assert(callback.calledOnce);
+    // });
+
+    it('calls callback after 100ms', function () {
+        var callback = sinon.spy();
+        var throttled = sinonTutorial.throttle(callback);
+
+        throttled();
+
+        clock.tick(99);
+        assert(callback.notCalled);
+
+        clock.tick(1);
+        assert(callback.calledOnce);
+
+        // Also:
+        // assert.equals(new Date().getTime(), 100);
     });
 });
